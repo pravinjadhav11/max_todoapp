0f00907885a3ac17d9cb26afced717b18f9b3ad4
Pravin Jadhav <askpravinjadhav@gmail.com>
2024-01-15 14:06:47 +0530

    initial

--- App.js
+++ App.js
@@ -1,20 +1,49 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+
+import { Button, StyleSheet, Text, TextInput, View } from 'react-native';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+  
+    <View style={styles.appContainer}>
+     <View style ={styles.inputContainer}>
+<TextInput style= {styles.TextInput} placeholder="Course Goal" />
+<Button title="ADD GOAL" />
+
+     </View>
+     <View style={styles.goalsContainer}>
+<Text>List Of Goal</Text>
+
+     </View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+ appContainer:{
+  flex:1,
+  paddingTop:50,
+  paddingHorizontal:16,
+
+ },
+ inputContainer:{
+  flex:1,
+   flexDirection:'row',
+   justifyContent:'space-between',
+   alignItems:'center',
+   marginBottom:24,
+   borderBottomColor:'#cccccc',
+   borderBottomWidth:1
+ },
+ TextInput:{
+   width:'70%',
+   borderColor:'#cccccc ',
+   borderWidth:1,
+   marginRight:8,
+   padding:10
+
+   
+ },
+ goalsContainer:{
+  flex:5
+ }
 });
